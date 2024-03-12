bf43b5e1bd90ece03ddfff0781588ed64ddce738
Peereeyut <109719689+Peereeyut@users.noreply.github.com>
2024-03-12 15:06:31 +0700

    api advisor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -159,7 +159,7 @@ export class HomeComponent implements OnInit {
       for (let i of resultData.data) {
         this.http.get(this.api + "/api/studentproject/:" + i.idProject).subscribe(async (resstudent: any) => {
           // console.log(res.data)
-          this.http.get(this.api + "/api/studentproject/:" + i.idProject).subscribe(async (resadvisor: any) => {
+          this.http.get(this.api + "/api/advisorproject/:" + i.idProject).subscribe(async (resadvisor: any) => {
             var setarray = await [i, resstudent.data, resadvisor.data]
           this.forgetstudent.push(await setarray)
           })
