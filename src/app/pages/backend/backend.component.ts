1df991573f7213b6d9780ddce0b58fd923293579
Peereeyut <109719689+Peereeyut@users.noreply.github.com>
2024-03-22 19:22:51 +0700

    Update backend.component.ts

--- src/app/pages/backend/backend.component.ts
+++ src/app/pages/backend/backend.component.ts
@@ -1,5 +1,4 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { FormBuilder } from '@angular/forms'
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -7,6 +6,9 @@ import { AuthService } from 'src/app/service/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-backendr',
   templateUrl: './backend.component.html',
@@ -14,19 +16,24 @@ import { Router } from '@angular/router';
 })
 export class BackendComponent implements AfterViewInit {
 
-  constructor(private builder: FormBuilder, private service: AuthService, private dialog: MatDialog, private router: Router) {
+  constructor( private service: AuthService, private dialog: MatDialog, private router: Router, private http:HttpClient, private toastr:ToastrService) {
     this.LoadUser();
   }
+  
   userlist: any;
   dataSource: any;
   sessionid: any;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  api = "https://backend-project-neon.vercel.app";
   varcategory:string=""
-  listcategory=['Computer Engineering', 'Electrical Engineering', 'Mechanical Engineering', 'Civil Engineering', 'Chemical Engineering', 'Industrial Engineering', 'Materials Engineering', 'Environmental Engineering']
+  listcategory:any[]=[];
+  userProfile:any
   ngAfterViewInit():void {
+    this.userProfile = JSON.parse(sessionStorage.getItem("loggedInUser") || "");
     this.sessionid = sessionStorage.getItem("id")?.toString();
-    this.listcategory.sort()
+    this.funcgetmajor();
+    // this.listcategory.sort()
 
   }
   LoadUser() {
@@ -65,5 +72,52 @@ export class BackendComponent implements AfterViewInit {
     
   }
 
+  async funcclearcategory(){
+    this.listcategory_insert=[]
+  }
 
+  async funcgetmajor(){
+    this.http.get(this.api+"/backend/major").subscribe(async(res:any)=>{
+      for(let item of res.data){
+        var name = item.major_name
+        this.listcategory.push(name)
+      }
+    })
+  }
+  
+  async funcSubmit(){
+    var time = Date().toString()
+    var date = await this.formatDate(time)
+    for(let i of this.listcategory_insert){
+
+      
+      var body={
+        "major_name":i,
+        "editedby":this.userProfile.email,
+        "editedby_time": date
+      }
+      console.log(body)
+      this.http.post(this.api+'/backend/major/add',body).subscribe((res:any)=>{
+        console.log(res)
+        if(res.status){
+          this.toastr.success("add major Complete")
+        }
+      })
+    }
+  }
+  
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = this.padZero(date.getMonth() + 1);
+    const day = this.padZero(date.getDate());
+    const hours = this.padZero(date.getHours());
+    const minutes = this.padZero(date.getMinutes());
+    const seconds = this.padZero(date.getSeconds());
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
+  padZero(num: number): string {
+    return num < 10 ? `0${num}` : `${num}`;
+  }
 }
